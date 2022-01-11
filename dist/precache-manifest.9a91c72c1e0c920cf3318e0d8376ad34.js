8ac2ca55b96b3a38c6c3c32f4a714e3d4ef9b6bb
emanuelfleitas <emanuelfleitas2020@gmail.com>
2022-01-11 15:18:45 -0300

    cambios

---
+++ dist/precache-manifest.9a91c72c1e0c920cf3318e0d8376ad34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a566fae1ee570e0d70be",
+    "url": "/css/app.f7d1e743.css"
+  },
+  {
+    "revision": "622fdce856c0e5c9115a9bfd6ea517b5",
+    "url": "/img/logo_rick_and_morty.622fdce8.png"
+  },
+  {
+    "revision": "e0ace688c9097010e525679ec3ee2009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a566fae1ee570e0d70be",
+    "url": "/js/app.03d8fbf4.js"
+  },
+  {
+    "revision": "4c918cf7b7186edbdeef",
+    "url": "/js/chunk-vendors.50b33eb4.js"
+  },
+  {
+    "revision": "61682cedf350be255e6b1517e9b5738f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
